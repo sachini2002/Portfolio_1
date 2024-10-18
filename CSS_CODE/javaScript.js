9923193a2ff4a69f1809e4936409a25257c8fccd
sachini <sachinipoornima7@gmail.com>
2024-10-18 16:46:51 +0000

    Done some changes

---
+++ CSS_CODE/javaScript.js
@@ -0,0 +1,36 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav ul li a');
+
+
+window.onscroll = ()=>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150 ;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >=offset && top < offset +height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('nav ul li a ' +
+                    '[href'+ id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+
+
+
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    const bannerHeight = document.querySelector('.banner').offsetHeight;
+
+    if (window.scrollY > bannerHeight) {
+        nav.classList.add('scrolled'); // Add class to make nav visible
+    } else {
+        nav.classList.remove('scrolled'); // Remove class to hide nav
+    }
+});
+
